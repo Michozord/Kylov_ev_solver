37e3857a3c18338330a9e3c5ee908e9073fe6288
Michozord <66361088+Michozord@users.noreply.github.com>
2024-01-19 16:37:34 +0100

    beta plots with tests

--- dff.py
+++ dff.py
@@ -6,13 +6,29 @@ Created on Thu Jan 18 13:20:39 2024
 """
 
 import numpy as np
+from matplotlib import pyplot as plt
+from typing import Callable
 
-def q_omega(omega, tau, M):
+def q_omega(omega: float, tau: float, M: int) -> np.array:
     q_omegas = [1., 1.]
     for l in range(1, M):
-        q_omegas += [(2-tau**2 * omega**2)*q_omegas[-1] - q_omegas[-2]]
-    return q_omegas
+        q_omegas.append((2-tau**2 * omega**2)*q_omegas[-1] - q_omegas[-2])
+    return np.array(q_omegas)
     
 
-def beta (alpha, omega, tau):
-    pass
+def beta (omega: float, alpha: np.array, tau: float) -> float:
+    q = q_omega(omega, tau, len(alpha)-1)
+    return tau * alpha @ q
+
+
+def plot_beta(start: float, end: float, alpha, tau, M, ax, label=None, absolute=True):
+    mesh = np.linspace(start, end, num=1000)
+    if isinstance(alpha, Callable):
+        alpha_vec = np.array(list(map(lambda l: alpha(tau*l), range(0, M+1))))
+    else:
+        alpha_vec = np.array(alpha)
+    abs_fun = abs if absolute else lambda x: x
+    ax.plot(mesh, list(map(lambda om: abs_fun(beta(om, alpha_vec, tau)), mesh)), label=label)
+        
+    
+
---
+++ dff_test.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 13:06:35 2024
+
+@author: Michal Trojanowski
+"""
+
+from dff import *
+import numpy as np
+from matplotlib import pyplot as plt
+from sympy_test import sympy_plot
+
+
+def alpha_constructor(T, omega_min, omega_max):
+    
+    def alpha(t):
+        if t > T:
+            return 0
+        elif t == 0:
+            return 2 * (omega_max-omega_min)/np.pi
+        elif 0 < t and t <= T:
+            return 4/(np.pi * t) * np.sin(t*(omega_max-omega_min)/2) * np.cos(t*(omega_max+omega_min)/2)
+        else:
+            return alpha(-t)
+        
+    return alpha
+
+
+omega_min = 2
+omega_max = 4
+start = 0
+end = 20
+Ts = [1, 2.5, 5, 10]
+tau = 0.025
+fig, ax = plt.subplots()
+for T in Ts:
+    alpha = alpha_constructor(T, omega_min, omega_max)
+    plot_beta(start, end, alpha, tau, int(T/tau), ax, "T="+str(T), absolute=True)  
+ax.grid()
+plt.xlim(start, end)
+plt.ylim(-0.5, 1.5)
+plt.legend()
+plt.show()
+    
+
+
+# omega_min = 2
+# omega_max = 4
+# start = 0
+# end = 20
+# T = 1
+# tau = 0.025
+# fig, ax = plt.subplots()
+# alpha = lambda t : t
+# plot_beta(start, end, alpha, tau, int(T/tau), ax, "T="+str(T), absolute=False)
+# mesh = np.linspace(start, end, num=1000)
+# val = tau**2 * np.array([np.array(range(41)) @ q_omega(omega, tau, 40) for omega in mesh])
+# ax.plot(mesh, val, label="ref")
+# ax.grid()
+# plt.xlim(start, end)
+# plt.ylim(-0.5, 1.5)
+# plt.legend()
+# plt.show()
+
+
+# omega_min = 2
+# omega_max = 4
+# start = 0
+# end = 20
+# T = 5
+# tau = 0.025
+# fig, ax = plt.subplots()
+# alpha = alpha_constructor(T, omega_min, omega_max)
+# plot_beta(start, end, alpha, tau, int(T/tau), ax, "plot_beta", absolute=False)
+# sympy_plot(alpha, ax, tau, int(T/tau), T)
+
+# ax.grid()
+# plt.xlim(start, end)
+# plt.ylim(-0.5, 1.5)
+# plt.legend()
+# plt.show()
+
+
+
