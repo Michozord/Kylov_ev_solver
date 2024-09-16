ba8dd584a0715077dd878c644e698e2faf3e3967
Michozord <66361088+Michozord@users.noreply.github.com>
2024-09-16 11:53:59 +0200

    Updated __all__ list

--- src/krylovevsolver/__init__.py
+++ src/krylovevsolver/__init__.py
@@ -4,6 +4,7 @@ Created on Tue Jun 25 13:33:01 2024
 
 @author: Michal Trojanowski
 """
-__all__ = ["krylov_solver"]
+__all__ = ["krylov_solver", "dff", "wave_equation", "KrylovSolver", "Results", "FilterType", "Filter", "FilterGenerator"]
 
 from .krylov_solver import *
+from .dff import *
